fbb46bc705af50a96e5ce5fc3b428f9881757853
MarkG <mark@grayzu.com>
2016-10-06 00:13:23 -0700

    Enable logic for handling file exist or not.

--- configuration/routes/getConfig.js
+++ configuration/routes/getConfig.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var getConfig = require('../bin/getConfig');
 var logger = require('winston');
 
@@ -40,23 +41,41 @@ router.use('/', function(req, res, next){
 // Process get config request
 router.get(getConfigPath, function(req, res, next) {
   logger.debug(`Configuration '${req.params.configName}' requested.`);
-  var responseCode = 200; //400 = BAD REQUEST, 404 = NOT FOUND
-  var configFileName = 'testConfig.mof';
-  var configFilePath = path.join(__dirname, `../${configFileName}`);
+  var configFileName = `${req.params.configName}.mof`;
+  logger.debug(`Path to configuration files: ${req.app.locals.config.configFilesPath}.`)
+  var configFilePath = path.join(req.app.locals.config.configFilesPath, `${configFileName}`);
   
-  //TODO: Handle different response scenarios: file exists, file does not exist, 
 
-  getConfig.getFileHash(configFilePath,'sha256', function(hash){
-    res.statusCode = responseCode;
-    res.header('Content-Type','application/json');
-    res.header('ProtocolVersion','2.0');
-    res.header('Checksum','hash');
-    res.header('ChecksumAlgorithm','SHA-256');
+  //TODO: Validate certificate if it exists. If cert exists, request did not come through proxy. Return 400 if cert invalid.
+  
+  
+  //Get statistics about configuration file to validate it exists. Error will be returned if file does not exists.
+  fs.stat(configFilePath,function(err, stats){ 
+    //Reply with configuration file and S_OK when config file exists.
+    if(!err){
+        getConfig.getFileHash(configFilePath,'sha256', function(hash){
+          res.statusCode = 200;
+          res.header('Content-Type','text/plain');
+          res.header('ProtocolVersion','2.0');
+          res.header('Content-Length', stats.size);
+          res.header('Checksum',hash);
+          res.header('ChecksumAlgorithm','SHA-256');
 
-    logger.debug(`Sending configuration file ${configFilePath}.`);
-    res.sendFile(configFilePath);
+          logger.debug(`Sending configuration file ${configFilePath}.`);
+          res.sendFile(configFilePath);
+        });
+
+    }
+    //Reply with Not Found 
+    else
+    {
+      res.statusCode = 404;
+      res.header('ProtocolVersion','2.0');
+
+      logger.info(`Could not find configuration file ${configFilePath}.`);
+      res.end();
+    }
   });
-
 });
 
 module.exports = router;
